da3d6bf404c6e887ca0dbabb77fd7e88b68998da
Arvish M <vurghus.minar@outlook.com>
2021-01-26 23:47:04 +0400

    removed kirki typography, added gutenberg support, added customizer preview, added theme mod integration styles integration

--- inc/customizer-custom-controls/assets/js/customizer-preview.js
+++ inc/customizer-custom-controls/assets/js/customizer-preview.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready(function($) {
 
 	// Change the font-size of body
-	wp.customize('typography_font_size_body', function(control) {
+	wp.customize('font_size_body', function(control) {
 		control.bind(function( controlValue ) {
 			$('body').css('font-size', controlValue + 'px');
 		});
@@ -9,35 +9,35 @@ jQuery( document ).ready(function($) {
 
 
 	// Change the font-size of site title
-	wp.customize('typography_font_size_site_title', function(control) {
+	wp.customize('font_size_site_title', function(control) {
 		control.bind(function( controlValue ) {
 			$('.site-title').css('font-size', controlValue + 'px');
 		});
 	});
 
 	// Change the font-size of site description
-	wp.customize('typography_font_size_site_desc', function(control) {
+	wp.customize('font_size_site_desc', function(control) {
 		control.bind(function( controlValue ) {
 			$('.site-description').css('font-size', controlValue + 'px');
 		});
 	});
 
 	// Change the font-size of Top level menu items
-	wp.customize('typography_font_size_menu_top_items', function(control) {
+	wp.customize('font_size_menu_top_items', function(control) {
 		control.bind(function( controlValue ) {
 			$('.main-navigation li').css('font-size', controlValue + 'px');
 		});
 	});
 
 	// Change the font-size of Submenu items
-	wp.customize('typography_font_size_menu_sub_items', function(control) {
+	wp.customize('font_size_menu_sub_items', function(control) {
 		control.bind(function( controlValue ) {
 			$('.main-navigation ul ul li').css('font-size', controlValue + 'px');
 		});
 	});
 
 	// Change the font-size of Post titles (archives)
-	wp.customize('typography_font_size_index_title', function(control) {
+	wp.customize('font_size_index_title', function(control) {
 		control.bind(function( controlValue ) {
 			$('.blog-loop .entry-title').css('font-size', controlValue + 'px');
 		});
@@ -45,7 +45,7 @@ jQuery( document ).ready(function($) {
 
 
 	// Change the font-size of Post titles (singles)
-	wp.customize('typography_font_size_single_title', function(control) {
+	wp.customize('font_size_single_title', function(control) {
 		control.bind(function( controlValue ) {
 			$('.single-post .entry-title').css('font-size', controlValue + 'px');
 		});
@@ -53,7 +53,7 @@ jQuery( document ).ready(function($) {
 
 
 	// Change the font-size of Widget titles
-	wp.customize('typography_font_size_widget_title', function(control) {
+	wp.customize('font_size_widget_title', function(control) {
 		control.bind(function( controlValue ) {
 			$('.widget-area .widget-title').css('font-size', controlValue + 'px');
 		});
@@ -61,31 +61,47 @@ jQuery( document ).ready(function($) {
 
 
 	// Change the font-size of Widget text
-	wp.customize('typography_font_size_widgets', function(control) {
+	wp.customize('font_size_widgets', function(control) {
 		control.bind(function( controlValue ) {
 			$('.widget-area .widget').css('font-size', controlValue + 'px');
 		});
 	});	
 
 	// Change the font-size of Footer widget titles
-	wp.customize('typography_font_size_footer_widget_titles', function(control) {
+	wp.customize('font_size_footer_widget_titles', function(control) {
 		control.bind(function( controlValue ) {
 			$('.sidebar-column .widget-title').css('font-size', controlValue + 'px');
 		});
 	});	
 
 	// Change the font-size of Footer widget text
-	wp.customize('typography_font_size_footer_widgets', function(control) {
+	wp.customize('font_size_footer_widgets', function(control) {
 		control.bind(function( controlValue ) {
 			$('.sidebar-column .widget').css('font-size', controlValue + 'px');
 		});
 	});	
 
 	// Change the font-size of Footer credits
-	wp.customize('typography_font_size_footer_credits', function(control) {
+	wp.customize('font_size_footer_credits', function(control) {
 		control.bind(function( controlValue ) {
 			$('.site-info').css('font-size', controlValue + 'px');
 		});
-	});	
+	});
+
+	// Change the headings font-family
+	wp.customize('headings_font', function(control) {
+		control.bind(function( controlValue ) {
+			$('h1, h2, h3, h4, h5, h6, .site-title').css('font-family', controlValue );
+		});
+	});
+
+	// Change the body font-family
+	wp.customize('body_font', function(control) {
+		control.bind(function( controlValue ) {
+			$('body, button, input, select, textarea').css('font-family', controlValue );
+		});
+	});
+
+	
 
 });
--- inc/customizer-custom-controls/customizer.php
+++ inc/customizer-custom-controls/customizer.php
@@ -43,7 +43,8 @@ class Airi_initialise_customizer_settings {
 		 */
 		 $wp_customize->add_panel( 'airi_typography_panel',
 		 	array(
-				'title' => __( 'Typography v2', 'airi' )
+				'priority' => 11,
+				'title' => __( 'Typography', 'airi' )
 			)
 		);
 	}
@@ -90,13 +91,13 @@ class Airi_initialise_customizer_settings {
 	 */
 	public function airi_register_typography_font_families_controls( $wp_customize ) {
 
-		$wp_customize->add_setting( 'typography_body_font',
+		$wp_customize->add_setting( 'body_font',
 			array(
-				'default' => $this->defaults['typography_body_font'],
+				'default' => $this->defaults['body_font'],
 				'sanitize_callback' => 'airi_google_font_sanitization'
 			)
 		);
-		$wp_customize->add_control( new Airi_Google_Font_Select_Custom_Control( $wp_customize, 'typography_body_font',
+		$wp_customize->add_control( new Airi_Google_Font_Select_Custom_Control( $wp_customize, 'body_font',
 			array(
 				'label' => __( 'Body', 'airi' ),
 				'section' => 'typography_font_families_section',
@@ -107,13 +108,13 @@ class Airi_initialise_customizer_settings {
 			)
 		) );
 
-		$wp_customize->add_setting( 'typography_headings_font',
+		$wp_customize->add_setting( 'headings_font',
 			array(
-				'default' => $this->defaults['typography_headings_font'],
+				'default' => $this->defaults['headings_font'],
 				'sanitize_callback' => 'airi_google_font_sanitization'
 			)
 		);
-		$wp_customize->add_control( new Airi_Google_Font_Select_Custom_Control( $wp_customize, 'typography_headings_font',
+		$wp_customize->add_control( new Airi_Google_Font_Select_Custom_Control( $wp_customize, 'headings_font',
 			array(
 				'label' => __( 'Headings', 'airi' ),
 				'section' => 'typography_font_families_section',
@@ -146,14 +147,14 @@ class Airi_initialise_customizer_settings {
 		) );
 		
 		// Body Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_body',
+		$wp_customize->add_setting( 'font_size_body',
 			array(
-				'default' => $this->defaults['typography_font_size_body'],
+				'default' => $this->defaults['font_size_body'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_body',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_body',
 			array(
 				'label' => __( 'Body', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -181,14 +182,14 @@ class Airi_initialise_customizer_settings {
 
 
 		// Site Title Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_site_title',
+		$wp_customize->add_setting( 'font_size_site_title',
 			array(
-				'default' => $this->defaults['typography_font_size_site_title'],
+				'default' => $this->defaults['font_size_site_title'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_site_title',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_site_title',
 			array(
 				'label' => __( 'Site title', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -201,14 +202,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Site description Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_site_desc',
+		$wp_customize->add_setting( 'font_size_site_desc',
 			array(
-				'default' => $this->defaults['typography_font_size_site_desc'],
+				'default' => $this->defaults['font_size_site_desc'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_site_desc',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_site_desc',
 			array(
 				'label' => __( 'Site description', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -221,14 +222,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Top Menu Items Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_menu_top_items',
+		$wp_customize->add_setting( 'font_size_menu_top_items',
 			array(
-				'default' => $this->defaults['typography_font_size_menu_top_items'],
+				'default' => $this->defaults['font_size_menu_top_items'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_menu_top_items',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_menu_top_items',
 			array(
 				'label' => __( 'Top menu items', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -241,14 +242,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Sub Menu Items Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_menu_sub_items',
+		$wp_customize->add_setting( 'font_size_menu_sub_items',
 			array(
-				'default' => $this->defaults['typography_font_size_menu_sub_items'],
+				'default' => $this->defaults['font_size_menu_sub_items'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_menu_sub_items',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_menu_sub_items',
 			array(
 				'label' => __( 'Submenu items', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -276,14 +277,14 @@ class Airi_initialise_customizer_settings {
 
 
 		// Post titles (archives) Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_index_title',
+		$wp_customize->add_setting( 'font_size_index_title',
 			array(
-				'default' => $this->defaults['typography_font_size_index_title'],
+				'default' => $this->defaults['font_size_index_title'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_index_title',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_index_title',
 			array(
 				'label' => __( 'Post titles (archives)', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -296,14 +297,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Post titles (singles) Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_single_title',
+		$wp_customize->add_setting( 'font_size_single_title',
 			array(
-				'default' => $this->defaults['typography_font_size_single_title'],
+				'default' => $this->defaults['font_size_single_title'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_single_title',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_single_title',
 			array(
 				'label' => __( 'Post titles (singles)', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -331,14 +332,14 @@ class Airi_initialise_customizer_settings {
 
 
 		// Widget titles Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_widget_title',
+		$wp_customize->add_setting( 'font_size_widget_title',
 			array(
-				'default' => $this->defaults['typography_font_size_widget_title'],
+				'default' => $this->defaults['font_size_widget_title'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_widget_title',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_widget_title',
 			array(
 				'label' => __( 'Widget titles', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -351,14 +352,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Widget text Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_widgets',
+		$wp_customize->add_setting( 'font_size_widgets',
 			array(
-				'default' => $this->defaults['typography_font_size_widgets'],
+				'default' => $this->defaults['font_size_widgets'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_widgets',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_widgets',
 			array(
 				'label' => __( 'Widget text', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -386,14 +387,14 @@ class Airi_initialise_customizer_settings {
 
 
 		// Footer widget titles Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_footer_widget_titles',
+		$wp_customize->add_setting( 'font_size_footer_widget_titles',
 			array(
-				'default' => $this->defaults['typography_font_size_footer_widget_titles'],
+				'default' => $this->defaults['font_size_footer_widget_titles'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_footer_widget_titles',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_footer_widget_titles',
 			array(
 				'label' => __( 'Footer widget titles', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -406,14 +407,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Footer widget text Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_footer_widgets',
+		$wp_customize->add_setting( 'font_size_footer_widgets',
 			array(
-				'default' => $this->defaults['typography_font_size_footer_widgets'],
+				'default' => $this->defaults['font_size_footer_widgets'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_footer_widgets',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_footer_widgets',
 			array(
 				'label' => __( 'Footer widget text', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -426,14 +427,14 @@ class Airi_initialise_customizer_settings {
 		) );
 
 		// Footer credits Font Size Slider Control
-		$wp_customize->add_setting( 'typography_font_size_footer_credits',
+		$wp_customize->add_setting( 'font_size_footer_credits',
 			array(
-				'default' => $this->defaults['typography_font_size_footer_credits'],
+				'default' => $this->defaults['font_size_footer_credits'],
 				'transport' => 'postMessage',
 				'sanitize_callback' => 'absint'
 			)
 		);
-		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'typography_font_size_footer_credits',
+		$wp_customize->add_control( new Airi_Slider_Custom_Control( $wp_customize, 'font_size_footer_credits',
 			array(
 				'label' => __( 'Footer credits', 'airi' ),
 				'section' => 'typography_font_sizes_section',
@@ -484,25 +485,25 @@ require_once trailingslashit( dirname(__FILE__) ) . 'custom-controls/class-airi-
  */
 $airi_settings = new Airi_initialise_customizer_settings(
 	array(
-		'typography_font_size_body' => 16,
-		'typography_font_size_site_title' => 36,
-		'typography_font_size_site_desc' => 16,
-		'typography_font_size_menu_top_items' => 13,
-		'typography_font_size_menu_sub_items' => 13,
-		'typography_font_size_index_title' => 30,
-		'typography_font_size_single_title' => 36,
-		'typography_font_size_widget_title' => 24,
-		'typography_font_size_widgets' => 16,
-		'typography_font_size_footer_widget_titles' => 20,
-		'typography_font_size_footer_widgets' => 16,
-		'typography_font_size_footer_credits' => 13,
-		'typography_body_font' => json_encode(
+		'font_size_body' => 16,
+		'font_size_site_title' => 36,
+		'font_size_site_desc' => 16,
+		'font_size_menu_top_items' => 13,
+		'font_size_menu_sub_items' => 13,
+		'font_size_index_title' => 30,
+		'font_size_single_title' => 36,
+		'font_size_widget_title' => 24,
+		'font_size_widgets' => 16,
+		'font_size_footer_widget_titles' => 20,
+		'font_size_footer_widgets' => 16,
+		'font_size_footer_credits' => 13,
+		'body_font' => json_encode(
 			array(
 				'font' => 'Work Sans',
 				'variant' => 'regular',
 			)
 		),
-		'typography_headings_font' => json_encode(
+		'headings_font' => json_encode(
 			array(
 				'font' => 'Work Sans',
 				'variant' => 'regular',
@@ -510,3 +511,209 @@ $airi_settings = new Airi_initialise_customizer_settings(
 		),
 	)
 );
+
+/**
+ * Enqueue Font scripts and styles.
+ * 
+ * @return void
+ */
+if ( ! function_exists( 'airi_font_scripts' ) ) {
+	function airi_font_scripts() {
+		$headings_font = esc_html(get_theme_mod('headings_font'));
+		$body_font = esc_html(get_theme_mod('body_font'));
+
+		if( $headings_font ) {
+			$font_pieces = airi_process_font($headings_font);
+			$font = $font_pieces[0] . ":" . $font_pieces[1];
+			wp_enqueue_style( 'airi-headings-fonts', '//fonts.googleapis.com/css?family='. htmlspecialchars_decode($font) , array(), null);
+		}
+
+		if( $body_font ) {
+			$font_pieces = airi_process_font($body_font);
+			$font = $font_pieces[0] . ":" . $font_pieces[1];
+			wp_enqueue_style( 'airi-body-fonts', '//fonts.googleapis.com/css?family='. htmlspecialchars_decode($font) , array(), null);
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'airi_font_scripts' );
+
+
+
+if ( ! function_exists( 'airi_font_styles' ) ) {
+	function airi_font_styles() {
+
+		$custom_styles = "";
+		$custom_styles .= airi_font_family_styles();
+		$custom_styles .= airi_font_sizes_styles();
+
+
+		// Output all styles
+		echo '<style id="airi-font-css">' . $custom_styles . '</style>';
+
+
+	}
+}
+add_action( 'wp_head', 'airi_font_styles', 9999 );
+
+
+
+if ( ! function_exists( 'airi_font_family_styles' ) ) {
+	function airi_font_family_styles() {
+		
+		$custom_styles = "";
+
+		$headings_font = esc_html(get_theme_mod('headings_font'));
+		$body_font = esc_html(get_theme_mod('body_font'));
+
+		if ( $headings_font ) {
+			
+			$font_pieces = $font_pieces = airi_process_font($headings_font);
+			$font = htmlspecialchars_decode($font_pieces[0]);
+			$custom_styles .= "h1, h2, h3, h4, h5, h6, .site-title { font-family: $font; }"."\n";
+		}
+
+		if ( $body_font ) {
+			$font_pieces = $font_pieces = airi_process_font($body_font);
+			$font = htmlspecialchars_decode($font_pieces[0]);
+			$custom_styles .= "body, button, input, select, textarea { font-family: $font; }"."\n";
+		}		
+		
+
+		return $custom_styles;
+	}
+}
+
+
+if ( ! function_exists( 'airi_gutenberg_font_family_styles' ) ) {
+	function airi_gutenberg_font_family_styles() {
+		
+		$custom_styles = "";
+
+		$headings_font = esc_html(get_theme_mod('headings_font'));
+		$body_font = esc_html(get_theme_mod('body_font'));
+
+		if ( $headings_font ) {
+			
+			$font_pieces = $font_pieces = airi_process_font($headings_font);
+			$font = htmlspecialchars_decode($font_pieces[0]);
+			$custom_styles .= ".editor-block-list__layout h1,.editor-block-list__layout h2,.editor-block-list__layout h3,.editor-block-list__layout h4,.editor-block-list__layout h5,.editor-block-list__layout h6,.editor-post-title__block .editor-post-title__input { font-family: $font; }"."\n";
+		}
+
+		if ( $body_font ) {
+			$font_pieces = $font_pieces = airi_process_font($body_font);
+			$font = htmlspecialchars_decode($font_pieces[0]);
+			$custom_styles .= ".editor-block-list__layout,.editor-block-list__layout .editor-block-list__block { font-family: $font; }"."\n";
+		}		
+		
+
+		return $custom_styles;
+	}
+}
+
+
+
+if ( ! function_exists( 'airi_font_sizes_styles' ) ) {
+	function airi_font_sizes_styles() {
+		$custom_styles = "";
+		
+		$font_size_body = absint( get_theme_mod( 'font_size_body' ) );
+		if ($font_size_body) {
+			$custom_styles .= "body { font-size: $font_size_body"."px; }"."\n";
+		}
+		
+		$font_size_site_title = absint( get_theme_mod( 'font_size_site_title' ) );
+		if ($font_size_site_title) {
+			$custom_styles .= ".site-title { font-size: $font_size_site_title"."px; }"."\n";
+		}
+
+		$font_size_site_desc = absint( get_theme_mod( 'font_size_site_desc' ) );
+		if ($font_size_site_desc) {
+			$custom_styles .= ".site-description { font-size: $font_size_site_desc"."px; }"."\n";
+		}		
+
+		$font_size_menu_top_items = absint( get_theme_mod( 'font_size_menu_top_items' ) );
+		if ($font_size_menu_top_items) {
+			$custom_styles .= ".main-navigation li { font-size: $font_size_menu_top_items"."px; }"."\n";
+		}
+		
+		$font_size_menu_sub_items = absint( get_theme_mod( 'font_size_menu_sub_items' ) );
+		if ($font_size_menu_sub_items) {
+			$custom_styles .= ".main-navigation ul ul li { font-size: $font_size_menu_sub_items"."px; }"."\n";
+		}		
+		
+		$font_size_index_title = absint( get_theme_mod( 'font_size_index_title' ) );
+		if ($font_size_index_title) {
+			$custom_styles .= ".blog-loop .entry-title { font-size: $font_size_index_title"."px; }"."\n";
+		}			
+		
+		$font_size_single_title = absint( get_theme_mod( 'font_size_single_title' ) );
+		if ($font_size_single_title) {
+			$custom_styles .= ".single-post .entry-title { font-size: $font_size_single_title"."px; }"."\n";
+		}		
+		
+		$font_size_widget_title = absint( get_theme_mod( 'font_size_widget_title' ) );
+		if ($font_size_widget_title) {
+			$custom_styles .= ".widget-area .widget-title { font-size: $font_size_widget_title"."px; }"."\n";
+		}	
+		
+		$font_size_widgets = absint( get_theme_mod( 'font_size_widgets' ) );
+		if ($font_size_widgets) {
+			$custom_styles .= ".widget-area .widget { font-size: $font_size_widgets"."px; }"."\n";
+		}			
+		
+		$font_size_footer_widget_titles = absint( get_theme_mod( 'font_size_footer_widget_titles' ) );
+		if ($font_size_footer_widget_titles) {
+			$custom_styles .= ".sidebar-column .widget-title { font-size: $font_size_footer_widget_titles"."px; }"."\n";
+		}		
+		
+		$font_size_footer_widgets = absint( get_theme_mod( 'font_size_footer_widgets' ) );
+		if ($font_size_footer_widgets) {
+			$custom_styles .= ".sidebar-column .widget { font-size: $font_size_footer_widgets"."px; }"."\n";
+		}			
+		
+		$font_size_footer_credits = absint( get_theme_mod( 'font_size_footer_credits' ) );
+		if ($font_size_footer_credits) {
+			$custom_styles .= ".site-info { font-size: $font_size_footer_credits"."px; }"."\n";
+		}
+
+		return $custom_styles;
+	}
+}
+
+
+if ( ! function_exists( 'airi_process_font' ) ) {
+
+	function airi_process_font($font_mod) {
+		$font_pieces = explode(",", str_replace(array("{", "}"), "", $font_mod));
+		$font_family = explode(":", $font_pieces[0]);
+		$font_variant = explode(":", $font_pieces[1]);
+
+		return array($font_family[1], $font_variant[1]);
+	}
+}
+
+
+/**
+ * Gutenberg
+ */
+if ( ! function_exists( 'airi_gutenberg_editor_font_styles' ) ) {
+	function airi_gutenberg_editor_font_styles() {	
+		airi_font_scripts();
+
+		$custom_styles = airi_gutenberg_font_family_styles();
+		wp_add_inline_style( 'airi-block-editor-styles', $custom_styles );
+	}
+}
+add_action( 'enqueue_block_editor_assets', 'airi_gutenberg_editor_font_styles' );
+
+/**
+ * Enqueue scripts for our Customizer preview
+ *
+ * @return void
+ */
+if ( ! function_exists( 'airi_customizer_preview_scripts' ) ) {
+	function airi_customizer_preview_scripts() {
+		wp_enqueue_script( 'airi-customizer-preview-js', trailingslashit( get_template_directory_uri() ) . 'inc/customizer-custom-controls/assets/js/customizer-preview.js', array( 'customize-preview', 'jquery' ), time(), false );
+	}
+}
+add_action( 'customize_preview_init', 'airi_customizer_preview_scripts' );
